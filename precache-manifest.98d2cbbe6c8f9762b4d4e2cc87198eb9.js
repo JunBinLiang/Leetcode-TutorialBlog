121a244abac0eb16d983d89567598f15460b79ed
junbinLiang <junbinliang816@gmail.com>
2020-05-20 03:58:18 -0400

    Updates

---
+++ precache-manifest.98d2cbbe6c8f9762b4d4e2cc87198eb9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bef4f84e1f646233fd5dc92b6f447714",
+    "url": "/Leetcode-TutorialBlog/static/media/twosum.bef4f84e.md"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/Leetcode-TutorialBlog/static/media/empty.d41d8cd9.md"
+  },
+  {
+    "revision": "5c7629b2c3ee51934bc7",
+    "url": "/Leetcode-TutorialBlog/static/js/runtime~main.5c7629b2.js"
+  },
+  {
+    "revision": "f974b88ffbb6287129a0",
+    "url": "/Leetcode-TutorialBlog/static/js/main.f974b88f.chunk.js"
+  },
+  {
+    "revision": "e8e17f154197256ef475",
+    "url": "/Leetcode-TutorialBlog/static/js/1.e8e17f15.chunk.js"
+  },
+  {
+    "revision": "f974b88ffbb6287129a0",
+    "url": "/Leetcode-TutorialBlog/static/css/main.6a7cf080.chunk.css"
+  },
+  {
+    "revision": "e8e17f154197256ef475",
+    "url": "/Leetcode-TutorialBlog/static/css/1.4a887acb.chunk.css"
+  },
+  {
+    "revision": "6aadcfaef79cc4cc325628aff95b4571",
+    "url": "/Leetcode-TutorialBlog/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.59d04fffa47770bdd1d213c1e4d63ad6.js"
+  "/Leetcode-TutorialBlog/precache-manifest.98d2cbbe6c8f9762b4d4e2cc87198eb9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Leetcode-TutorialBlog/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
