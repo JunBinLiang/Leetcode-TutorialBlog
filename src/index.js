afc2dc45a0861ed1b154aa109652607f647d22a1
junbinLiang <junbinliang816@gmail.com>
2020-11-24 01:51:49 -0500

    a

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>,
+		
+			<App />,
+		
   document.querySelector('#root')
 );
 
