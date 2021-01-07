c1a241ab6dae5bb1281ff759a8104207bdca7c43
liulanz <zhengliulan@gmail.com>
2021-01-07 01:01:20 -0500

    add calendar page with random data

---
+++ src/Component/Calendar.js
@@ -0,0 +1,41 @@
+import { ResponsiveCalendar } from "@nivo/calendar";
+import { data } from "./data/calendarData";
+import React, { Component, useState } from "react";
+
+class Calendar extends Component {
+  // constructor() {
+  //   super();
+  // }
+
+  render() {
+    return (
+      <div style={{ height: "1000px", width: "10000px" }} className="container">
+        <ResponsiveCalendar
+          data={data}
+          from="2015-03-01"
+          to="2016-07-12"
+          emptyColor="#eeeeee"
+          colors={["#61cdbb", "#97e3d5", "#e8c1a0", "#f47560"]}
+          margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+          yearSpacing={40}
+          monthBorderColor="#ffffff"
+          dayBorderWidth={2}
+          dayBorderColor="#ffffff"
+          legends={[
+            {
+              anchor: "bottom-right",
+              direction: "row",
+              translateY: 36,
+              itemCount: 4,
+              itemWidth: 42,
+              itemHeight: 36,
+              itemsSpacing: 14,
+              itemDirection: "right-to-left",
+            },
+          ]}
+        />
+      </div>
+    );
+  }
+}
+export default Calendar;
