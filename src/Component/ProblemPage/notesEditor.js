f04f1423f6971c33a9f3e6340278d6f1257a940e
junbinLiang <junbinliang816@gmail.com>
2021-01-16 02:11:59 -0500

    update

--- src/Component/ProblemPage/notesEditor.js
+++ src/Component/ProblemPage/notesEditor.js
@@ -20,23 +20,38 @@ class NotesEditor extends Component {
     super();
     this.state = { value: mkdStr };
     this.change = this.change.bind(this);
+    this.scrollToBottom=this.scrollToBottom.bind(this);
   }
+ 
+
 
   change(val) {
     this.setState({ value: val });
   }
 
+  componentDidUpdate(prevProps, prevState){
+    this.scrollToBottom();
+  }
+
+  scrollToBottom() {
+    const {thing} = this.refs;
+    thing.scrollTop = thing.scrollHeight - thing.clientHeight;
+  }
+  
+
+
   render() {
     return (
       <div className="container">
-        <div className="itemconfiguration margin">
+        <div className="itemconfiguration margin" ref={`thing`} > 
           <MDEditor.Markdown
             source={this.state.value}
-            style={{ minHeight: "500px" }}
+            style={{ minHeight: "350px" }}
             className="notes"
           />
         </div>
 
+        <br/>
         <MDEditor
           height={300}
           value={this.state.value}
